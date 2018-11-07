8a215d39f85b6c00900888cc540f48df26e18352
Horneac Emanuel <horneac.emanuel@yahoo.com>
2018-11-07 19:56:04 +0200

    changed django-database-url to dj-database-url

--- rottenPotatoes/settings.py
+++ rottenPotatoes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django-database-url
+import dj-database-url
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
