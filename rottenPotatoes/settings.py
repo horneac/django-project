2b353b1943d8007f7f396be81d7d1c840044aeda
Horneac Emanuel <horneac.emanuel@yahoo.com>
2018-11-07 20:09:53 +0200

    smh

--- rottenPotatoes/settings.py
+++ rottenPotatoes/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-db_from_env = os.environ.get('DATABASE_URL')
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,7 +99,8 @@ DATABASES = {
         }
     }
 
-
+db_from_env = os.environ.get('DATABASE_URL')
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
