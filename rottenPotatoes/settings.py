6f0ec378074f6ef0b1c0bf4a4740607a9b7d8c6b
Horneac Emanuel <horneac.emanuel@yahoo.com>
2018-11-07 19:54:09 +0200

    modified settings to add database_url from environment

--- rottenPotatoes/settings.py
+++ rottenPotatoes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django-database-url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
