d4a05b004fdb798128a783fee563d1a3ec2e99a7
Horneac Emanuel <horneac.emanuel@yahoo.com>
2018-11-07 18:58:50 +0200

    fixed bug in settings:databases

--- rottenPotatoes/settings.py
+++ rottenPotatoes/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'rottenPotatoes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'rottenPotatoes',
         'USER': 'root',
         'PASSWORD': 'password',
